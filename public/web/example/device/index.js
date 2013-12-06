ab913f4f9a9e083cd774a1391a816913ffbcbb45
Eric Burns <eric.burns@lmco.com>
2013-12-06 16:38:35 -0500

    Have Device box show up when a new device connects to device app

--- public/web/example/device/index.js
+++ public/web/example/device/index.js
@@ -1,10 +1,50 @@
-vwf_view.ticked = function() {
-    //Query to see if any devices are connected
-}
+( function() {
+    var $deviceList;
 
-vwf_view.firedEvent = function ( nodeID, eventName, eventParameters ) {
-    if ( eventName === "deviceAdded" ) {
-        //Query the device node to get information about it
-        var dummy = 0;
-    }
-} //@ sourceURL=index.js
+    //Find device list element when page loads
+    //And in case it has already loaded, do the find now
+    window.onload = function() {
+        $deviceList = $("#device-list");
+        //$deviceList = document.getElementById("#device-list");
+    };
+    $deviceList = $("#device-list");
+    //$deviceList = document.getElementById("#device-list");    
+
+    vwf_view.ticked = function() {
+        //Query to see if any devices are connected
+    };
+
+    vwf_view.firedEvent = function ( nodeID, eventName, eventParameters ) {
+        if ( eventName === "deviceConnected" ) {
+
+            $deviceList.append(
+                "      <li>\n" +
+                "        <form>\n" +
+                "          <h2>Device Name</h2>\n" +
+                "          <h3>Properties</h3>\n" +
+                "          <ul>\n" +
+                "            <li>Property Name: <input type='text' value='3'></li>\n" +
+                "          </ul>\n" +
+                "          <h3>Methods</h3>\n" +
+                "          <ul>\n" +
+                "            <li>Method Name:</li>\n" +
+                "            <ul>\n" +
+                "              <li>Parameter Name: <input type='text' placeholder='3'></li>\n" +
+                "              <li><button class='btn'>Call</button></li>\n" +
+                "            </ul>\n" +
+                "          </ul>\n" +
+                "          <h3>Events</h3>\n" +
+                "          <ul>\n" +
+                "            <li>Event Name: Last called at 13:03 on 12-4-2013</li>\n" +
+                "          </ul>\n" +
+                "        </form>\n" +
+                "      </li>"
+            );
+
+            // var deviceID = eventParameters[ 1 ];
+            // vwf_view.kernel.getProperties( deviceID, 0, function( props ) {
+            //     for ( var prop in props ) {}
+            // } );
+        }
+    };
+} )(); //@ sourceURL=index.js
