6ebb83d77b496bcebffa62c5fc1e8ad60235b4ba
unknown <chadwickr@Rob-PC.vacolab.adlnet.org>
2014-03-20 17:59:09 -0400

    change scale of terrain to better work with walk mode speed

--- support/client/lib/vwf/model/threejs.js
+++ support/client/lib/vwf/model/threejs.js
@@ -411,7 +411,7 @@ define( [ "module", "vwf/model", "vwf/utility", "vwf/utility/color", "jquery" ],
             }
 
             if ( myNode && myNode.terrain) {
-                debugger; 
+               
                 myNode.terrain.initializingNode();
             }
         },
